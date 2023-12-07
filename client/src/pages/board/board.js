9809c34462386a07303150eb52d24ba28470d48e
KenCorp <kennethwarui35@gmail.com>
2023-12-07 17:12:20 +0300

    made cards column and card move

--- client/src/pages/board/board.js
+++ client/src/pages/board/board.js
@@ -4,13 +4,28 @@ import useBoard from '../../state/board'
 import './board.css'
 const BoardPage = () => {
     const {board, setBoard} = useBoard()
+
+    const handleColumnMove = (__card, source, destination) => {
+        const updateBoard = moveColumn(board, source, destination)
+        setBoard(updateBoard)
+    }
+    const handleCardMove = (__card, source, destination) => {
+        const updateBoard = moveCard(board, source, destination)
+            setBoard(updateBoard)
+    }
     return ( 
         <div className='boardX'>
         
         <span>
             Trello Board
         </span>
-        <KanabanBoard>
+        <KanabanBoard 
+                        allowAddColumn
+                        allowRenameColumn
+                        allowRemoveCard
+                        onCardDragEnd={handleCardMove}
+                        onColumnDragEnd={handleColumnMove}
+        >
                 {board}
         </KanabanBoard>
         </div>
