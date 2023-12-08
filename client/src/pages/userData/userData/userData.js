cebef2d3365a8669008eb801941b329615873fdd
KenCorp <kennethwarui35@gmail.com>
2023-12-08 14:38:51 +0300

    created users

--- client/src/pages/userData/userData/userData.js
+++ client/src/pages/userData/userData/userData.js
@@ -52,6 +52,59 @@ const data = [
     city: 'Charleston',
     state: 'South Carolina',
   },
+  {
+    name: {
+      firstName: 'Charlse',
+      lastName: 'Mwaura',
+    },
+    address: '722 Emie Stream',
+    city: 'Lincoln',
+    state: 'Nebraska',
+  },
+  {
+    name: {
+      firstName: 'Joshua',
+      lastName: 'Rolluffs',
+    },
+    address: '32188 Larkin Turnpike',
+    city: 'Charleston',
+    state: 'South Carolina',
+  }, {
+    name: {
+      firstName: 'Jane',
+      lastName: 'Doe',
+    },
+    address: '769 Dominic Grove',
+    city: 'Columbus',
+    state: 'Ohio',
+  },
+  {
+    name: {
+      firstName: 'Ron',
+      lastName: 'Mwangi',
+    },
+    address: '566 Brakus Inlet',
+    city: 'South Linda',
+    state: 'West Virginia',
+  },
+  {
+    name: {
+      firstName: 'Kevin',
+      lastName: 'Vandy',
+    },
+    address: '722 Emie Stream',
+    city: 'Lincoln',
+    state: 'Nebraska',
+  },
+  {
+    name: {
+      firstName: 'Joshua',
+      lastName: 'Rolluffs',
+    },
+    address: '32188 Larkin Turnpike',
+    city: 'Charleston',
+    state: 'South Carolina',
+  },
 ];
 
 const UserDataX = () => {
