fb4998653aeebc0fd4a977ada15d3d1e86427f73
KenCorp <kennethwarui35@gmail.com>
2023-12-08 14:32:48 +0300

    created userData component

--- client/src/pages/userData/userData/userData.js
+++ client/src/pages/userData/userData/userData.js
@@ -1,36 +1,102 @@
-import React, { useMemo } from "react";
-import  { MaterialReactTable} from 'material-react-table'
-import './userData.css'
 
-const UserData = () => {
-    const columns = useMemo(() => [
-            {
-                accessorKey: "name.firstName",
-                header: 'First Name',
-            },
-            {
-                accessorKey: "name.lastName",
-                header: 'Last Name',
-            },
-            {
-                accessorKey: "address",
-                header: "Address",
-            },
-            {
-                accessorKey: "city",
-                header: "City",
-            },
-            {
-                accessorKey: "state",
-                header: "State",
-            },
-    
-    ], [],);
-    return ( 
-        <div className="table">
-       <MaterialReactTable  colums= {columns}/>
-        </div>
-     );
-}
- 
-export default UserData;
+import { useMemo } from 'react';
+import './userData.css'
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+} from 'material-react-table';
+
+const data = [
+  {
+    name: {
+      firstName: 'Kenneth',
+      lastName: 'Warui',
+    },
+    address: '261 Erdman Ford',
+    city: 'East Daphne',
+    state: 'Kentucky',
+  },
+  {
+    name: {
+      firstName: 'Christine',
+      lastName: 'Juma',
+    },
+    address: '769 Dominic Grove',
+    city: 'Columbus',
+    state: 'Ohio',
+  },
+  {
+    name: {
+      firstName: 'Joe',
+      lastName: 'Doe',
+    },
+    address: '566 Brakus Inlet',
+    city: 'South Linda',
+    state: 'West Virginia',
+  },
+  {
+    name: {
+      firstName: 'Kevin',
+      lastName: 'Vandy',
+    },
+    address: '722 Emie Stream',
+    city: 'Lincoln',
+    state: 'Nebraska',
+  },
+  {
+    name: {
+      firstName: 'Joshua',
+      lastName: 'Rolluffs',
+    },
+    address: '32188 Larkin Turnpike',
+    city: 'Charleston',
+    state: 'South Carolina',
+  },
+];
+
+const UserDataX = () => {
+  //should be memoized or stable
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: 'name.firstName', //access nested data with dot notation
+        header: 'First Name',
+        size: 150,
+      },
+      {
+        accessorKey: 'name.lastName',
+        header: 'Last Name',
+        size: 150,
+      },
+      {
+        accessorKey: 'address', //normal accessorKey
+        header: 'Address',
+        size: 200,
+      },
+      {
+        accessorKey: 'city',
+        header: 'City',
+        size: 150,
+      },
+      {
+        accessorKey: 'state',
+        header: 'State',
+        size: 150,
+      },
+    ],
+    [],
+  );
+
+  const table = useMaterialReactTable({
+    columns,
+    data, //data must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
+  });
+
+  return (
+  <div className='tableX'>
+      <MaterialReactTable table={table} />
+  </div>
+  );
+};
+
+export default UserDataX;
