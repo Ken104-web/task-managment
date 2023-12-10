60ac36fcedfc5566ceefd1b5f63c457154eae062
KenCorp <kennethwarui35@gmail.com>
2023-12-10 19:05:04 +0300

    created user and relationship model

--- server/models.py
+++ server/models.py
@@ -1,6 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    avatar = db.Column(db.String)
+
+    tasks = db.relationship('Task', secondary='task_users', back_populates='users')
+
 class Task(db.Model):
     __tablename__ = 'tasks'
 
@@ -9,10 +18,11 @@ class Task(db.Model):
     status = db.Column(db.String)
     time = db.Column(db.Integer)
 
-    def to_dict(self):
-        return{
-            'id': self.id,
-            'name': self.name,
-            'status': self.status,
-            'time': self.time
-        }
+    users = db.relationship('User', secondary='task-users', back_populates='tasks')
+
+class TaskUser(db.Model):
+    __tablename__ = 'task_users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    task_id = db.Column(db.Integer, db.ForeignKey('tasks.id'))
