485479f28cbed4a50d6eec18a2418f9560a38aaa
KenCorp <kennethwarui35@gmail.com>
2023-12-10 14:13:20 +0300

    created models on server side

---
+++ server/models.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    status = db.Column(db.String)
+    time = db.Column(db.Integer)
