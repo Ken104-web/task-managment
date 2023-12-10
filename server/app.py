1fddd8b278aa45a1a24da631b144527797da7268
KenCorp <kennethwarui35@gmail.com>
2023-12-10 18:01:32 +0300

    made successful get request

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import db
+from flask_restful import Resource, Api
 from flask_migrate import Migrate
 
 app = Flask(__name__)
@@ -11,4 +12,19 @@ app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
+
+api = Api(app)
+
+
+class Home(Resource):
+    def get(self):
+        resp_dict = {
+            "Home": "Welcome to Task Manager"
+        }
+        resp = make_response(
+            jsonify(resp_dict),
+            200,
+        )
+        return resp
+api.add_resource(Home, '/')
 
