e676e7edf215c1639f33dd49bfbd428621c5bd2a
KenCorp <kennethwarui35@gmail.com>
2023-12-10 17:54:44 +0300

    seeded task

---
+++ server/seed.py
@@ -0,0 +1,27 @@
+from app import app
+from models import db, Task
+import random
+
+with app.app_context():
+    Task.query.delete()
+
+
+    tasks= [
+        'Add Prototype on invision',
+        'Make a login flow in figma',
+        'Documentation for food delivery',
+        'Add multiselection for Travel-story',
+        'Create Detail view for Travel-story',
+        'Watch Jujitsu Kaisen',
+    ]
+    task = []
+    for i in range(6):
+        print("**Alive**")
+        assignTask = Task(
+            name = tasks[i],
+            status = random.choice(['In progress', 'In Review', 'Done', 'To Do']),
+            time = random.randint(1,12))
+        print('**still works**')
+        task.append(assignTask)
+        db.session.add_all(task)
+        db.session.commit()
